7cbe96495abc5cf66d0717a18d18fea507488d66
yucemerkal <102432824+yucemerkal@users.noreply.github.com>
2026-09-19 14:26:55 +0300

    Update UsageStatsHelper.kt

--- app/src/main/java/com/dijitalkalkan/app/data/UsageStatsHelper.kt
+++ app/src/main/java/com/dijitalkalkan/app/data/UsageStatsHelper.kt
@@ -25,13 +25,22 @@ data class AppUsage(
  */
 object UsageStatsHelper {
 
+    @Suppress("DEPRECATION")
     fun hasUsagePermission(context: Context): Boolean {
         val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
-        val mode = appOps.unsafeCheckOpNoThrow(
-            AppOpsManager.OPSTR_GET_USAGE_STATS,
-            Process.myUid(),
-            context.packageName
-        )
+        val mode = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            appOps.unsafeCheckOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(),
+                context.packageName
+            )
+        } else {
+            appOps.checkOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(),
+                context.packageName
+            )
+        }
         return mode == AppOpsManager.MODE_ALLOWED
     }
 
